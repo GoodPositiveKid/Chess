c5a54475aa36b3dfb9cad3d3047b588e7ebb3e84
Minglang Du <mldu@cydu.net>
2021-08-12 13:25:15 -0700

    add queens and rooks

--- other.py
+++ other.py
@@ -5,8 +5,8 @@ class Spritesheet:
     def __init__(self,filename):
         self.path = str(pathlib.Path(__file__).parent.absolute())
         self.sheet = pygame.image.load(self.path + '/' + filename).convert()
-    def image(self,x,y,width,height,colorkey):
+    def image(self,x,y,width,height):
         image = pygame.Surface([width, height]).convert()
         image.blit(self.sheet, (0, 0), (x, y, width, height))
-        image.set_colorkey(colorkey)
+        image.set_colorkey((255,255,255))
         return image
--- pieces.py
+++ pieces.py
@@ -3,7 +3,7 @@ from pygame.sprite import Sprite
 from other import Spritesheet
 class Piece(Sprite):
     """The base class for a chess piece"""
-    def __init__(self,game,x,y,board,color):
+    def __init__(self, color, game, x,y, board):
         self.gridx = x
         self.gridy = y
         self.board = board
@@ -19,6 +19,20 @@ class King(Piece):
     def __init__(self,color,game,x,y,board):
         super().__init__(game, x, y, board, color)
         if (color == "Black"):
-            self.image = self.spritesheet.image(70,72,85,85,(255,255,255))
+            self.image = self.spritesheet.image(70,72,85,85)
         else:
-            self.image = self.spritesheet.image(70,215,85,85,(255,255,255))
+            self.image = self.spritesheet.image(70,215,85,85)
+class Queen(Piece):
+    def __init__(self, color, game, x,y, board):
+        super().__init__(color,game, x, y, board)
+        if (color == "Black"):
+            self.image = self.spritesheet.image(233,72,85,85)
+        else:
+            self.image = self.spritesheet.image(233,215,85,85)
+class Rook(Piece):
+    def __init__(self, color, game, x, y, board):
+        super().__init__(color, game, x, y, board)
+        if (color == "Black"):
+            self.image = self.spritesheet.image(410,72,85,85)
+        else:
+            self.image = self.spritesheet.image(410,215,85,85)
